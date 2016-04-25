3c87e2d9f8917f615f642533a62a56d2e9eb789c
servkey <luis.montane@hotmail.com>
2016-04-25 09:55:49 -0500

    Constante.

--- Prácticas/práctica 1 - Conceptos Básicos/Main.java
+++ Prácticas/práctica 1 - Conceptos Básicos/Main.java
@@ -1,6 +1,9 @@
 public class Main{
+	final int x = 10;
+		
 	public static void main(String[] args){
 		//Construcción de un objeto Trabajador
+
 		Trabajador t = new Trabajador("Juan","juan@uv.mx");	
 		//t.mostrar();
 	
