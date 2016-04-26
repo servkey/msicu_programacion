9ae591828e2502f676ea613d4f8c1e312547e064
servkey <luis.montane@hotmail.com>
2016-04-26 15:23:11 -0500

    Ejercicios.

--- Prácticas/práctica 1 - Conceptos Básicos/Main.java
+++ Prácticas/práctica 1 - Conceptos Básicos/Main.java
@@ -3,7 +3,6 @@ public class Main{
 		
 	public static void main(String[] args){
 		//Construcción de un objeto Trabajador
-
 		Trabajador t = new Trabajador("Juan","juan@uv.mx");	
 		//t.mostrar();
 	
